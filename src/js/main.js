2fc924406a17bcef4e257569262a91e37dab666f
artem <mstrJesm@gmail.com>
2020-11-29 23:04:12 +0200

    finished desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 		})
 	}
 
-	navToggler('header-nav-btn', 'header-nav'); 
+	navToggler('header-nav-btn', 'header-nav-list'); 
 
 	$('.about-slider').slick({
 		slidesToShow: 1,
@@ -21,12 +21,70 @@ $(document).ready(function() {
 		nextArrow: `<button type="button" class="slider-arr right-arr">Next</button>`
 	});
 
+	$('.more').on('click', function() {
+		$(this).addClass('isLoading');
 
-	$('.projects-row').isotope({
-	  	masonry: {
-		    gutter: 25,
-		    columnWidth: '.cart'
-		    //fitWidth: true
-	  	}
-	})
+		setTimeout(() => {
+			$(this).removeClass('isLoading');
+			$.getJSON('js/data.json', function(data){
+		  	var container = $('.projects-row');
+		  	var items = [];
+
+		  	$(data).each((i, item) => {
+		  		items.push(` 
+		  			<article class="cart" id="${item.id}">
+        			<main class="cart-pic">
+                <div class="cart-controlls">
+                	<a href="#" class="cart-view">Show more</a>
+                	<button class="cart-favourite">Add to favourite</button>
+                </div>
+                <img src="images/${item.pic}" alt="">
+              </main>
+              <footer class="cart-desc">
+                <h4>${item.title}</h4>
+                <p><em>${item.label}</em></p>
+              </footer>
+            </article>
+		  		`)
+		  	});
+
+		  	container.append(items);
+			});
+		}, 2000);
+		
+	}); 
+
+
+	let btn = $('.subscribe-form-btn');
+	$(".subscribe-form input[type='email']").keyup(function(){
+    var email = $(this).val();
+
+    if(email != 0) {
+      if(isValidEmailAddress(email)) {
+      	getFormState.call(this, "green", false);
+      } else {
+        getFormState.call(this, "#b82222", true);
+      }
+    } else {
+      getFormState.call(this, "transparent", false);     
+    }
+
+  });
+
+  function getFormState(borderColor, btnState) {
+  	$(this).css({
+      "border-color": borderColor
+    });
+
+    btn.attr('disabled', btnState);
+  }
+
+
+	function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+	}
+
+	
+
 })
